ee3637e67df6c6651c96fd79f6d5e3a11d68adf5
Wendy Kopf <wendykopf@Wendys-MacBook-Pro.local>
2020-02-01 20:28:14 -0500

    Adding insertion and retrieval of mock data. These files are for example.

---
+++ retrieveData.py
@@ -0,0 +1,32 @@
+import sqlite3 
+
+def readSqliteTable():
+  try:
+    sqliteConnection = sqlite3.connect('Recipes.db')
+    cursor = sqliteConnection.cursor()
+    print ("Connection to SQLite")
+
+    sqlite_select_query = """SELECT * from Recipe"""
+    cursor.execute(sqlite_select_query)
+    records = cursor.fetchall()
+    print("Total rows are: ", len(records))
+    print("Printing each row")
+    for row in records:
+      print("ID", row[0])
+      print("Name", row[1])
+      print("Description", row[2])
+      print("Ingredients", row[3])
+      print("Instructions", row[4])
+      print("Category", row[5])
+
+    cursor.close()
+  
+  except sqlite3.Error as error:
+  	print("Failed to read data from sqlite table ", error)
+  
+  finally:
+  	if (sqliteConnection):
+  		sqliteConnection.close()
+  		print("The SQLite connection is closed")
+
+readSqliteTable()
